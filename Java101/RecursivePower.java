6ce10bf7e78b0991b23184758f43387f58a2aed4
kariba38 <mseyyitkilic@gmail.com>
2022-06-07 18:22:18 +0300

    first

---
+++ Java101/RecursivePower.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RecursivePower {
+    static int power(int a, int b) {
+        if (b == 0)
+            return 1;
+
+        return a * power(a, b - 1);
+    }
+
+    public static void main(String[] args) {
+        int taban, us;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Taban degerini giriniz: ");
+        taban = sc.nextInt();
+        System.out.print("Us degerini giriniz: ");
+        us = sc.nextInt();
+        sc.close();
+
+        System.out.println(power(taban, us));
+    }
+}
